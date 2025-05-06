ce28d38faf40f0ea27010a669ed29a84a764348b
joelharris181106 <joel.harris@kalvium.community>
2025-05-06 18:54:19 +0530

    backend deployed

--- Backend/Routes/userroutes.js
+++ Backend/Routes/userroutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../Models/users');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const { signup, signin } = require('../Controllers/authcontrollers');
